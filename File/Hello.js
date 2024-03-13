600e086b70db84de12ecf2bd4aa6e44cf2feeb5e
Khoa Tran <khoatran3005@gmail.com>
2024-03-13 10:05:27 -0400

    workshop

---
+++ File/Hello.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+// export default class Hello extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <div>Hello world from class component</div>
+//         )
+//     }
+// }
+
+const Hello = () => {
+    return (
+        <div>
+            Hello world from functional component
+        </div>
+    );
+}
+
+export default Hello;
